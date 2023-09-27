bb15855f62f21ae0042b8ba2e8c2a8934e35e253
666666bg <65243723+666666bg@users.noreply.github.com>
2023-09-27 17:11:00 +0200

    переніс секрети з images api до secrets

--- modules/Images/images_api.py
+++ modules/Images/images_api.py
@@ -1,31 +1,31 @@
 from imgurpython import ImgurClient
 
-CLIENT_ID = 'f82bcb68847441a'
-CLIENT_SECRET = 'd3870caebcf64e7cc4ab0d180204d25b292dc3b6'
+from modules.auth_manager import AuthManager
 
 class ImagesApi:
-    # Ваші дані з облікового запису Imgur
-    client_id = 'f82bcb68847441a'
-    client_secret = 'd3870caebcf64e7cc4ab0d180204d25b292dc3b6'
-    # завантаження фото з onedrive
+    def __init__(self):
+        self.auth_manager = AuthManager()
+    def start(self):
+        imgur_client_id = self.auth_manager.get_imgur_client_id()
+        imgur_client_secret = self.auth_manager.get_imgur_client_secret()
 
-    # Шлях до фото на вашому локальному комп'ютері
-    image_path = 'C:\\Users\\vladi\\OneDrive\\Desktop\\1.jpg'
+        # Шлях до фото на вашому локальному комп'ютері
+        image_path = 'C:\\Users\\vladi\\OneDrive\\Desktop\\1.jpg'
 
-    # Функція для завантаження фото на Imgur і отримання URL
-    def upload_image_to_imgur(image_path):
-        client = ImgurClient(CLIENT_ID, CLIENT_SECRET)
+        # Функція для завантаження фото на Imgur і отримання URL
+        def upload_image_to_imgur(image_path):
+            client = ImgurClient(imgur_client_id, imgur_client_secret)
 
-        # Завантаження фото
-        uploaded_image = client.upload_from_path(image_path, anon=True)
+            # Завантаження фото
+            uploaded_image = client.upload_from_path(image_path, anon=True)
 
-        # Отримання URL завантаженого фото
-        image_url = uploaded_image['link']
+            # Отримання URL завантаженого фото
+            image_url = uploaded_image['link']
 
-        return image_url
+            return image_url
 
-    # Завантаження фото і отримання URL
-    imgur_image_url = upload_image_to_imgur(image_path)
+        # Завантаження фото і отримання URL
+        imgur_image_url = upload_image_to_imgur(image_path)
 
-    # Виведення URL
-    print("URL фото на Imgur:", imgur_image_url)
+        # Виведення URL
+        print("URL фото на Imgur:", imgur_image_url)
